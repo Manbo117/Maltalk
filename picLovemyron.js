3b551abc81913a8e4dac22e12e8a2fc89f2fbe45
Manbo <56032741+Manbo117@users.noreply.github.com>
2020-11-28 15:07:41 +0800

    Upload by PicGo

---
+++ picLovemyron.js
@@ -0,0 +1,4 @@
+// 页脚跳动的心
+$(document).ready(function(e){
+    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> By Myron');
+})
